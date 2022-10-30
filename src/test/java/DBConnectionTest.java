96fbf42ab608ecddd0d901e4475586e2a2e495fe
Djimmka <dimsti94@gmail.com>
2022-10-30 16:28:06 +0300

    УДАЛИЛ НА ВСЯКИЙ СЛУЧАЙ РЕАЛИЗАЦИЮ ДЛЯ СЛЕДУЮЩЕГО ЗАДАНИЯ HIBERNATE КОТОРАЯ МОГЛА БЫТЬ С АНАЛОГИЧНЫМИ ОШИБКАМИ И СМУЩАТЬ НА ПРОВЕРКЕ
    если ошибки для задания по JDBC - всё ещё не исправлены - требую развёрнутых пояснений по всем пунктам.
    
     Создание таблицы для User(ов) – не должно приводить к исключению, если такая таблица уже существует
     Удаление таблицы User(ов) – не должно приводить к исключению, если таблицы не существует
    Выполнить эти 2 требования через sql запрос ( if (not) exist - не достаточно?! почему?!)
    
    Для очистки таблицы есть более эффективная sql комманда (что эффективнее truncate?!)
    
    Почему Connection закрывается не в try с ресурсами? (но оно ЗАКРЫВАЕТСЯ!!! даже идея пишет, что connect.close() - избыточен)
    
    Запросы писать в методах(убрать переменные) (надо всё перевести в вид "prepared statement"?)
    
    Обрабатывать исключения в методах дао - было неисправлено (какая именно обработка нужна? повторная попытка выполнить команду?)

--- src/test/java/DBConnectionTest.java
+++ src/test/java/DBConnectionTest.java
@@ -1,7 +1,5 @@
 public class DBConnectionTest {
-    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_3-4_jdbc_hibernate-master";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+
     public static void main(String[] args) {
         UserServiceTest usTest = new UserServiceTest();
         usTest.createUsersTable();
